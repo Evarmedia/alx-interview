4ffcc64eba063461d393c4b2304dd773563952f5
Evarmedia <mishakmanuel@gmail.com>
2024-04-24 11:32:03 +0100

    style: pycodestyle was used to correct code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,14 +2,13 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = []
-    for i in range(n):
-        row = []
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                row.append(1)
-            else:
-                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+    triangle = [[1]]  # Initialize with the first row
+
+    for i in range(1, n):
+        row = [1]  # Each row starts with 1
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)  # Each row ends with 1
         triangle.append(row)
 
     return triangle
