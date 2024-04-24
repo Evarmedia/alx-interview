2809c4149ba84571827fdf3e62f4f4e4f30e0ee3
Evarmedia <mishakmanuel@gmail.com>
2024-04-24 11:40:41 +0100

    test: Test seems to be woking fine but checker still shows a requirement or output error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
+
 def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Initialize with the first row
-
-    for i in range(1, n):
-        row = [1]  # Each row starts with 1
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)  # Each row ends with 1
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         triangle.append(row)
 
     return triangle
