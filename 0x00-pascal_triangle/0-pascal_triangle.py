6970d1ff5ccd6977b43b72009bbf7594168520e8
Evarmedia <mishakmanuel@gmail.com>
2024-04-25 17:39:19 +0100

    pasicalyTriangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,14 +3,24 @@
 0-pascal_triangle
 """
 def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle of depth n.
+
+    Args:
+    - n: An integer representing the depth of the Pascal's Triangle.
+
+    Returns:
+    - A list of lists of integers representing Pascal's Triangle.
+    """
     if n <= 0:
         return []
 
     triangle = []
     for i in range(n):
-        row = [1] * (i + 1)  # Initialize the row with 1s
-        for j in range(1, i):  # Calculate values inside the row (excluding edges)
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        row = [1] * (i + 1)
+        if i > 1:
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
 
     return triangle
