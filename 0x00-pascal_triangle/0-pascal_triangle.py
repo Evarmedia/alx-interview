8d262104c1e873f224a29a946e51d8abf23f5137
Evarmedia <mishakmanuel@gmail.com>
2024-04-25 17:09:07 +0100

    pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
+
 def pascal_triangle(n):
     if n <= 0:
         return []
 
     triangle = []
     for i in range(n):
-        row = []
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                row.append(1)
-            else:
-                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row = [1] * (i + 1)  # Initialize the row with 1s
+        for j in range(1, i):  # Calculate values inside the row (excluding edges)
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
 
     return triangle
